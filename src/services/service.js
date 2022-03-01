53d2a07910115fe98967271f0ce49740e6f86eca
yejianbin <yejb_01@163.com>
2022-03-01 09:37:01 +0800

    fix(): queryString 引用去除

--- src/services/service.js
+++ src/services/service.js
@@ -1,18 +1,23 @@
 import { post } from "../utils/request";
-import testTree from "../../../node/graphql/MBT/queryString/testTree";
-import testItem from "../../../node/graphql/MBT/queryString/testItem";
-import testJob from "../../../node/graphql/MBT/queryString/testJob";
 
+// TODO queryString
 /**
  * 获取测试结果树
  * @param {Object} params
  */
 export function findTestTreeList({ testTaskId }) {
-  const query = testTree.findTestTreeList({
-    search: { testTaskId },
-    alias: "findTestTreeList",
-    result: "id,suiteTree",
-  });
+  const query = `
+            findTestTreeList: findTestTreeList(
+                search: {testTaskId:"${testTaskId}"}
+            ){
+                success
+                message
+                data{
+                    id,
+                    suiteTree
+                }
+            }
+        `;
 
   return post(`query {${query}}`).then((res) => res.data.findTestTreeList.data);
 }
@@ -22,11 +27,17 @@ export function findTestTreeList({ testTaskId }) {
  * @param {Object} params
  */
 export function getTestItemDetail({ id }) {
-  const query = testItem.getTestItemDetail({
-    search: { id },
-    alias: "getTestItemDetail",
-    result: "id,name,type,flag,level,retries,duration,testResult{data},testStepList{name,content}",
-  });
+  const query = `
+          getTestItemDetail:getTestItemDetail(
+                  search:{id:"${id}"},
+              ){
+                  success
+                  message
+                  data {
+                    id,name,type,flag,level,retries,duration,testResult{data},testStepList{name,content}
+                  }
+              }
+          `;
 
   return post(`query {${query}}`).then((res) => res.data);
 }
@@ -36,11 +47,17 @@ export function getTestItemDetail({ id }) {
  * @param {Object} params
  */
 export function getTestTaskDetail({ testTaskId }) {
-  const query = testJob.getTestTaskDetail({
-    search: { id: testTaskId },
-    alias: "getTestTreeDetail",
-    result: "id,env,duration,startTime,endTime,testJobId,statistics{blocker,critical,normal,minor,trivial}",
-  });
+  const query = `
+            getTestTreeDetail:getTestTaskDetail(
+                search:{id:"${testTaskId}"},
+                ){
+                    success
+                    message
+                    data {
+                      id,env,duration,startTime,endTime,testJobId,statistics{blocker,critical,normal,minor,trivial}
+                    }
+                }
+            `;
 
   return post(`query {${query}}`).then((res) => res.data.getTestTreeDetail.data);
 }
@@ -51,12 +68,18 @@ export function getTestTaskDetail({ testTaskId }) {
  * @param {String} params.testJobId 测试任务id
  */
 export function findTestTaskList({ testJobId }) {
-  const query = testJob.findTestTaskList({
-    search: { testJobId },
-    alias: "findTestTaskList",
-    sort: { execNo: "DESC" },
-    result: "execNo,statistics{passes, pending, blocker, critical, normal, minor, trivial,total}",
-  });
+  const query = `
+            findTestTaskList:findTestTaskList(
+                search:{testJobId:"${testJobId}"},
+                sort:{execNo: "DESC"}
+                ){
+                    success
+                    message
+                    data {
+                      execNo,statistics{passes, pending, blocker, critical, normal, minor, trivial,total}
+                    }
+                }
+            `;
 
   return post(`query {${query}}`).then((res) => res.data.findTestTaskList.data);
 }
