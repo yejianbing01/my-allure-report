40b1622d020be68276016312ddb902768c1fd664
yejianbin <yejb_01@163.com>
2022-03-30 16:35:36 +0800

    fix(): 增加预发环境

--- config/serviceUrl.js
+++ config/serviceUrl.js
@@ -2,8 +2,8 @@ const { REACT_APP_SERVICE_ENV } = process.env;
 
 const url = {
   local: "http://localhost:6673/MBT",
-  dev: "http://192.168.0.123:6674/MBT",
-  pre: "http://localhost:6673/MBT",
+  test: "http://192.168.0.123:6674/MBT",
+  pre: "http://192.168.0.123:6673/MBT",
 };
 
 export const serviceURL = url[REACT_APP_SERVICE_ENV || "local"];
