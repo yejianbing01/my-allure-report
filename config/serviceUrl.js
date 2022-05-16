17f8a7bfab94372403db0dc60c5d04b78d42e9d8
yejianbin <yejb_01@163.com>
2022-05-16 17:15:10 +0800

    feat(): 生产环境配置

--- config/serviceUrl.js
+++ config/serviceUrl.js
@@ -4,6 +4,7 @@ const url = {
   local: "http://localhost:6673/MBT",
   test: "http://192.168.0.123:6674/MBT",
   pre: "http://192.168.0.123:6673/MBT",
+  production: "http://119.3.0.226:6673/MBT",
 };
 
 export const serviceURL = url[REACT_APP_SERVICE_ENV || "local"];
