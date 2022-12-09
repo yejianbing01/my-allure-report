dc0cb2462b5e0f8c2da53a9d6b3dac2d8fe3f525
yejianbin <yejb_01@163.com>
2022-12-09 19:38:42 +0800

    feat(): 增加apifox测试结果显示

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -7,6 +7,8 @@ import HistoryTrend from "./components/HistoryTrend";
 import DataList from "./components/DataList";
 import { durationToString } from "../../utils/utils";
 import "./style.css";
+import Tree from "../../components/Tree";
+import { featuresAddStatus as addStatus, featuresSubStatus as subStatus, didTestCase } from "../../redux/actions";
 
 Dashboard.propTypes = {
   testJobId: PropTypes.string,
@@ -20,6 +22,11 @@ Dashboard.propTypes = {
   failedNumList: PropTypes.array,
   skipNumList: PropTypes.array,
   findTestTaskList: PropTypes.func,
+  apiFoxResult: PropTypes.object,
+  addStatus: PropTypes.func,
+  subStatus: PropTypes.func,
+  didTestCase: PropTypes.func,
+  statusList: PropTypes.array,
 };
 /** 看板页面 */
 function Dashboard({
@@ -34,28 +41,64 @@ function Dashboard({
   failedNumList,
   skipNumList,
   findTestTaskList,
+  apiFoxResult: { apiFoxTotalTestCaseNum, apiFoxTotalNumList, apiFoxTestItemList },
+  addStatus,
+  subStatus,
+  didTestCase,
+  statusList,
 }) {
   useEffect(() => {
     testJobId && findTestTaskList({ testJobId });
   }, [testJobId]);
 
+  useEffect(() => {
+    return () => didTestCase();
+  }, []);
+
   return (
     <div className="dashboard">
       <div className="content">
         <div className="widgets-grid">
-          <div className="widgets-grid_col">
-            <Summary
-              title={testJobName ? `${testJobName}(${serverURL})` : ""}
-              time={endTime ? `${startTime} ~ ${endTime}(${durationToString(duration)})` : startTime}
-              {...{ totalTestCaseNum, totalNumList }}
-            />
-            <DataList title={"测试套"} dataList={totalDataList} link={`/testSuite?testTaskId=${testTaskId}`} />
-            <DataList title={"分类"} dataList={featuresDataList} link={`/feature?testTaskId=${testTaskId}`} />
-          </div>
-          <div className="widgets-grid_col">
-            <HistoryTrend {...{ historyXAxis, passNumList, failedNumList, skipNumList }} />
-            <DataList title={"错误类别"} dataList={failedDataList} link={`/feature?testTaskId=${testTaskId}`} />
-          </div>
+          {apiFoxTotalTestCaseNum ? (
+            <>
+              <div className="widgets-grid_col">
+                <Summary
+                  title={"基于模型测试"}
+                  subTitle={testJobName ? `${testJobName}(${serverURL})` : ""}
+                  time={endTime ? `${startTime} ~ ${endTime}(${durationToString(duration)})` : startTime}
+                  {...{ totalTestCaseNum, totalNumList }}
+                />
+                <DataList title={"测试套"} dataList={totalDataList} link={`/testSuite?testTaskId=${testTaskId}`} />
+                <DataList title={"分类"} dataList={featuresDataList} link={`/feature?testTaskId=${testTaskId}`} />
+                <HistoryTrend {...{ historyXAxis, passNumList, failedNumList, skipNumList }} />
+                <DataList title={"错误类别"} dataList={failedDataList} link={`/feature?testTaskId=${testTaskId}`} />
+              </div>
+              <div className="widgets-grid_col">
+                <Summary
+                  title={testJobName ? `接口冒烟测试` : ""}
+                  time={endTime ? `${startTime} ~ ${endTime}(${durationToString(duration)})` : startTime}
+                  {...{ totalTestCaseNum: apiFoxTotalTestCaseNum, totalNumList: apiFoxTotalNumList }}
+                />
+                <Tree {...{ dataList: apiFoxTestItemList, totalNumList: apiFoxTotalNumList, statusList, addStatus, subStatus }} />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="widgets-grid_col">
+                <Summary
+                  title={testJobName ? `${testJobName}(${serverURL})` : ""}
+                  time={endTime ? `${startTime} ~ ${endTime}(${durationToString(duration)})` : startTime}
+                  {...{ totalTestCaseNum, totalNumList }}
+                />
+                <DataList title={"测试套"} dataList={totalDataList} link={`/testSuite?testTaskId=${testTaskId}`} />
+                <DataList title={"分类"} dataList={featuresDataList} link={`/feature?testTaskId=${testTaskId}`} />
+              </div>
+              <div className="widgets-grid_col">
+                <HistoryTrend {...{ historyXAxis, passNumList, failedNumList, skipNumList }} />
+                <DataList title={"错误类别"} dataList={failedDataList} link={`/feature?testTaskId=${testTaskId}`} />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
@@ -64,8 +107,9 @@ function Dashboard({
 
 export default connect(
   ({
-    testTreeReducer: { testJobId, testTaskId, summaryData, totalDataList, featuresDataList, failedDataList },
+    testTreeReducer: { testJobId, testTaskId, summaryData, totalDataList, featuresDataList, failedDataList, apiFoxResult },
     dashboardReducer: { historyXAxis, passNumList, failedNumList, skipNumList },
+    testStatusReducer: { statusList },
   }) => ({
     testJobId,
     testTaskId,
@@ -77,6 +121,8 @@ export default connect(
     passNumList,
     failedNumList,
     skipNumList,
+    apiFoxResult,
+    statusList,
   }),
-  { findTestTaskList }
+  { findTestTaskList, addStatus, subStatus, didTestCase }
 )(Dashboard);
--- src/services/service.js
+++ src/services/service.js
@@ -48,18 +48,18 @@ export function getTestItemDetail({ id }) {
  */
 export function getTestTaskDetail({ testTaskId }) {
   const query = `
-            getTestTreeDetail:getTestTaskDetail(
+            getTestTaskDetail:getTestTaskDetail(
                 search:{id:"${testTaskId}"},
                 ){
                     success
                     message
                     data {
-                      id,duration,startTime,endTime,testJobId,testJobName,serverURLName,serverURL,statistics{blocker,critical,normal,minor,trivial}
+                      id,duration,startTime,endTime,testJobId,testJobName,serverURLName,serverURL,statistics{blocker,critical,normal,minor,trivial},apiFoxResultList
                     }
                 }
             `;
 
-  return post(`query {${query}}`).then((res) => res.data.getTestTreeDetail.data);
+  return post(`query {${query}}`).then((res) => res.data.getTestTaskDetail.data);
 }
 
 /**
