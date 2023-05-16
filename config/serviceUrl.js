dee7cdc322eaeb07550689094ed63b626a5272b1
yejianbing01 <51230636+yejianbing01@users.noreply.github.com>
2023-05-16 16:46:19 +0800

    Update serviceUrl.js
    删除服务器配置

--- config/serviceUrl.js
+++ config/serviceUrl.js
@@ -2,9 +2,9 @@ const { REACT_APP_SERVICE_ENV } = process.env;
 
 const url = {
   local: "http://localhost:6673/MBT",
-  test: "http://192.168.0.123:6674/MBT",
-  pre: "http://192.168.0.123:6673/MBT",
-  production: "http://119.3.0.226:6673/MBT",
+  test: "",
+  pre: "",
+  production: "",
 };
 
 const serviceURL = url[REACT_APP_SERVICE_ENV || "local"];
